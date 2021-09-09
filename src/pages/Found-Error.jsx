25703a438f28038ef0a4cac1b0cf29a2e403e602
_rubns_ <ruben.sanchezr@alumno.buap.mx>
2021-09-08 19:25:07 -0500

    add mail

--- src/pages/Found-Error.jsx
+++ src/pages/Found-Error.jsx
@@ -16,6 +16,7 @@ export default function Found() {
                 <ContainerLading
                 git= "https://github.com/Avb-rubns"
                 link="https://www.linkedin.com/in/rub%C3%A9n-s%C3%A1nchez-rugerio-025588205/"
+                mail="mailto:rubns.s24@gmail.com"
                 >
                     <FiMail />
                     <FiGithub />
